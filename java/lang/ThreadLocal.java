1c9726b3f6d58f7ec81cf6188543fbca7c009cb6
GXQ <243184473@qq.com>
2021-11-28 23:48:34 +0800

    update

--- java/lang/ThreadLocal.java
+++ java/lang/ThreadLocal.java
@@ -157,9 +157,21 @@ public class ThreadLocal<T> {
      * @return the current thread's value of this thread-local
      */
     public T get() {
+        // 调用 Thread 类中的 currentThread() 方法，获取当前线程
         Thread t = Thread.currentThread();
+
+        // 获取当前线程（即 Thread 类实例）中的数据类型为 ThreadLocalMap 类的成员变量 threadLocals
+        //（该 threadLocals 即为存放 Entry 类实例的容器（可以等价的理解为存放 ThreadLocal 类实例的容器）
+        //（具体见 ThreadLocal 笔记中的 “ThreadLocal<T> 类的结构图”）
         ThreadLocalMap map = getMap(t);
+
+        // 如果该成员变量 threadLocals 不为空
+        // 表示此时当前线程中已经有使用过 ThreadLocal 来存放变量了
         if (map != null) {
+            // 调用 ThreadLocalMap 类的 getEntry() 方法，从 Thread 类中的成员变量 threadLocals（即 Entry 类实例的容器）中
+            // 获取该 ThreadLocal 类实例所对应的 Entry 类实例
+            // 从而获取存放在该 Entry 类实例中的变量（即 所谓的，存放在 ThreadLocal 中的变量）
+            // 并返回该变量
             ThreadLocalMap.Entry e = map.getEntry(this);
             if (e != null) {
                 @SuppressWarnings("unchecked")
@@ -167,6 +179,10 @@ public class ThreadLocal<T> {
                 return result;
             }
         }
+
+        // 如果为空，表示此时是第一次在当前线程中使用 ThreadLocal 类实例存放变量
+        // 此时
+        // 就调用 setInitialValue() 方法设置初始化值
         return setInitialValue();
     }
 
@@ -177,13 +193,40 @@ public class ThreadLocal<T> {
      * @return the initial value
      */
     private T setInitialValue() {
+        // 调用 initialValue() 方法获取初始值（即 需要存放在 ThreadLocal 中的变量的初始值）
+        //（该 initialValue() 方法就是我们会重写的方法）
         T value = initialValue();
+
+        // 调用 Thread 类中的 currentThread() 方法，获取当前线程
         Thread t = Thread.currentThread();
+
+        // 获取当前线程（即 Thread 类实例）中的数据类型为 ThreadLocalMap 类的成员变量 threadLocals
+        //（该 threadLocals 即为存放 Entry 类实例的容器（可以等价的理解为存放 ThreadLocal 类实例的容器）
+        //（具体见 ThreadLocal 笔记中的 “ThreadLocal<T> 类的结构图”）
         ThreadLocalMap map = getMap(t);
+
+
+        // 如果该成员变量 threadLocals 不为空
+        // 表示此时当前线程中已经有使用过 ThreadLocal 来存放变量了
         if (map != null)
+            // 调用 ThreadLocalMap 类中的 set() 方法来设置当前这个需要存放到 ThreadLocal 中的变量（即 形参 value）
+            // 其实
+            // 该方法内部就是往数据类型为 Entry 类的数组中存放元素
+            //（该元素（即 Entry 类实例）即为 Key 为 ThreadLocal 类实例，Value 为当前这个变量（即 形参 value）的键值对）
+            //（Entry 类的介绍见 ThreadLocal 笔记）
+
+            // 注意
+            // 这里的实参 this 就是该 ThreadLocal 类实例
             map.set(this, value);
         else
+            // 如果为空，表示此时是第一次在当前线程中使用 ThreadLocal 类实例存放变量
+            // 因此
+            // 就要创建 ThreadLocalMap 类实例这个容器，并把该容器赋值给 Thread 类中的成员变量 threadLocals
+            //（即 相当于初始化当前线程（即 Thread 类实例）的成员变量 threadLocals）
+            // 然后
+            // 往该容器中存放对当前这个变量（即 当前要往 ThreadLocal 中存放的变量）
             createMap(t, value);
+        
         return value;
     }
 
@@ -197,11 +240,33 @@ public class ThreadLocal<T> {
      *        this thread-local.
      */
     public void set(T value) {
+        // 调用 Thread 类中的 currentThread() 方法，获取当前线程
         Thread t = Thread.currentThread();
+
+        // 获取当前线程（即 Thread 类实例）中的数据类型为 ThreadLocalMap 类的成员变量 threadLocals
         ThreadLocalMap map = getMap(t);
+
+        // 如果当前该 ThreadLocalMap 类实例不为空
+        // 此时
+        // 表明当前线程中已经有使用过 ThreadLocal 来存放变量了，因为 ThreadLocalMap 这个容器中已经有了值
+        //（具体可见 ThreadLocal 笔记中的 “ThreadLocal<T> 类的结构图”）
         if (map != null)
+            // 调用 ThreadLocalMap 类中的 set() 方法来设置当前这个需要存放到 ThreadLocal 中的变量（即 形参 value）
+            // 其实
+            // 该方法内部就是往数据类型为 Entry 类的数组中存放元素
+            //（该元素（即 Entry 类实例）即为 Key 为 ThreadLocal 类实例，Value 为当前这个变量（即 形参 value）的键值对）
+            //（Entry 类的介绍见 ThreadLocal 笔记）
+
+            // 注意
+            // 这里的实参 this 就是该 ThreadLocal 类实例
             map.set(this, value);
         else
+            // 如果为空，表示此时是第一次在当前线程中使用 ThreadLocal 类实例存放变量
+            // 因此
+            // 就要创建 ThreadLocalMap 类实例这个容器，并把该容器赋值给 Thread 类中的成员变量 threadLocals
+            //（即 相当于初始化当前线程（即 Thread 类实例）的成员变量 threadLocals）
+            // 然后
+            // 往该容器中存放对当前这个变量（即 当前要往 ThreadLocal 中存放的变量）
             createMap(t, value);
     }
 
@@ -223,6 +288,9 @@ public class ThreadLocal<T> {
      }
 
     /**
+     * 此方法用于获取 Thread 类实例（即 线程）中的成员变量 threadLocals
+     * 该成员变量的数据类型就是 ThreadLocalMap（具体见 ThreadLocal 类笔记）
+     *
      * Get the map associated with a ThreadLocal. Overridden in
      * InheritableThreadLocal.
      *
@@ -241,6 +309,12 @@ public class ThreadLocal<T> {
      * @param firstValue value for the initial entry of the map
      */
     void createMap(Thread t, T firstValue) {
+        // 调用 ThreadLocalMap 类的构造方法，用于存放 ThreadLocal 类实例，以及对应的需要在当前线程中保存的变量副本
+        // 其实
+        // 该方法内部就是往数据类型为 Entry 类的数组（该数组即为 ThreadLocalMap 的成员变量 table）中存放元素（即 Entry 类实例）
+        //（该 Entry 类实例，即为 Key 为 ThreadLocal 类实例，Value 为当前这个变量（即 形参 value）的键值对）
+        //（Entry 类的介绍见 ThreadLocal 笔记）
+        // 这里的实参 this 就是当前这个 ThreadLocal 类实例
         t.threadLocals = new ThreadLocalMap(this, firstValue);
     }
 
@@ -307,6 +381,8 @@ public class ThreadLocal<T> {
          */
         static class Entry extends WeakReference<ThreadLocal<?>> {
             /** The value associated with this ThreadLocal. */
+            // 该成员变量指向的就是我们存放在 ThreadLocal 中的值
+            //（即 我们所谓的把值存放到 ThreadLocal 中，就是指把值赋值给它）
             Object value;
 
             Entry(ThreadLocal<?> k, Object v) {
@@ -317,12 +393,15 @@ public class ThreadLocal<T> {
 
         /**
          * The initial capacity -- MUST be a power of two.
+         *
+         * 该成员变量为该下面成员变量 table （即 存放 Entry 类实例的数组（容器））的初始容量（该值 16 为 2 的幂）
          */
         private static final int INITIAL_CAPACITY = 16;
 
         /**
          * The table, resized as necessary.
          * table.length MUST always be a power of two.
+         * 该数组的大小必须为 2 的幂（即如果该该数组扩容（其实就是新建一个数组，因为数组本身是大小固定的）它的大小必须为 2 的幂）
          */
         private Entry[] table;
 
@@ -345,6 +424,10 @@ public class ThreadLocal<T> {
 
         /**
          * Increment i modulo len.
+         *
+         * 该方法用于获取成员变量 table（数据类型为 Entry 类的数组）的下一个下标（即 槽位）
+         * 从该三目运算符可以看出，该数组是一个逻辑上的环形数组
+         *（因为 如果下一个下标越界了，那么就取 0（也就是数组第一个下标））
          */
         private static int nextIndex(int i, int len) {
             return ((i + 1 < len) ? i + 1 : 0);
@@ -449,7 +532,11 @@ public class ThreadLocal<T> {
          * Set the value associated with key.
          *
          * @param key the thread local object
+         *            形参 key 为当前这个 ThreadLocal 类实例
+         *           （即 ThreadLocal 类中的 set() 方法调用该 set() 方法时，传入的 this 指针）
+         *
          * @param value the value to be set
+         *              形参 value 就是当前要往 ThreadLocal 中存放的变量（即 需要线程隔离的变量）
          */
         private void set(ThreadLocal<?> key, Object value) {
 
@@ -458,22 +545,61 @@ public class ThreadLocal<T> {
             // it is to replace existing ones, in which case, a fast
             // path would fail more often than not.
 
+            // 获取成员变量 table（数据类型为 Entry 类的数组）（即 存放 ThreadLocal\<T> 类实例的容器）
             Entry[] tab = table;
             int len = tab.length;
+
+            // 计算槽位（即 数组的下标）
             int i = key.threadLocalHashCode & (len-1);
 
-            for (Entry e = tab[i];
-                 e != null;
-                 e = tab[i = nextIndex(i, len)]) {
+            // 从当前槽位（即 数组的下标）开始循环遍历数组，直到找到合适的槽位，放入 Entry 类实例
+            // 这里的 “合适” 是指
+            // 1. 当前槽位就是该 ThreadLocal 类实例所在的槽位
+            //   （即 当前调用该 set() 方法的目的是为了覆盖原先存放在该 ThreadLocal 中的变量）
+            // 2. 当前槽位所对应的 Entry 类实例已经“不新鲜”（即 下面 replaceStaleEntry() 方法名中的 stale）
+            //   （“不新鲜” 是指该 Entry 类实例所指向的 ThreadLocal 类实例已经被 GC 回收）
+            //    那么
+            //    说明此时该槽位所对应的 Entry 类实例已经没有用了
+            //    此时
+            //    就可以把新的值（即当前调用该 set() 方法所要设置的值（即 当要存放到 ThreadLocal 中的值））转换成一个新的 Entry 类实例
+            //    替换掉 “不新鲜” 的 Entry 类实例
+            for (Entry e = tab[i]; e != null; e = tab[i = nextIndex(i, len)]) {
+                // 获取当前 Entry 类实例所指向的 ThreadLocal 类实例
                 ThreadLocal<?> k = e.get();
 
+                // 如果该 ThreadLocal 类实例就是当前这个 ThreadLocal 类实例
+                //（说明此时调用这个 set() 方法是为了替换原先存放在这个 ThreadLocal 中的变量）
+                // 那么
+                // 就直接用新值（即 当前调用该 set() 方法需要设置的值（即 当要存放到 ThreadLocal 中的值））覆盖它对应的旧值
                 if (k == key) {
+                    // 使用新值（即 当前调用该 set() 方法需要设置的值（即 当要存放到 ThreadLocal 中的值））覆盖旧值
                     e.value = value;
+
+                    // 完成后直接返回
                     return;
                 }
 
+                // 如果当前 Entry 类实例所指向的 ThreadLocal 类实例为 null
+                //（代码走到这里这里，表示上面的 if 语句没走到，说明当前没在容器 table 中找到）
+                // 那么
+                // 就表示该 ThreadLocal 类实例以及被 GC 回收了（即 该 ThreadLocal 类实例所在的 Entry 类实例已经不新鲜了）
+                // 此时
+                // 就可以使用新的值（即当前调用该 set() 方法所要设置的值）转换成一个新的 Entry 类实例替换掉 “不新鲜” 的 Entry 类实例
+
+                // 注意
+                // 代码走到这里这里，表示上面的 if 语句没走到
+                // 即
+                // 说明此时调用这个 set() 方法 “可能” 不是为了替换原先存放在这个 ThreadLocal 中的变量
+                // 之所以说是可能
+                // 因为此时容器 table 还没遍历完
+                // 这也就是为什么下面调用的 replaceStaleEntry() 方法中会有和上面这个 if 语句相同逻辑的代码
+                // 就是为了在遍历容器 table 中的剩余元素时，对这种可能性再次进行验证
                 if (k == null) {
+                    // 把新的值（即当前调用该 set() 方法所要设置的值（即 当要存放到 ThreadLocal 中的值））转换成一个新的 Entry 类实例
+                    // 替换掉 “不新鲜” 的 Entry 类实例
                     replaceStaleEntry(key, value, i);
+
+                    // 完成后直接返回
                     return;
                 }
             }
@@ -512,10 +638,36 @@ public class ThreadLocal<T> {
          * "run" containing the stale entry.  (A run is a sequence of entries
          * between two null slots.)
          *
+         * 上面注释的大致含义是
+         *
+         * 把新的值（即当前调用该 set() 方法所要设置的值（即 当要存放到 ThreadLocal 中的值））转换成一个新的 Entry 类实例
+         * 替换掉 “不新鲜” 的 Entry 类实例
+         * 并且
+         * 该方法中还有一个 “副作用”，即会 “清理” 当前这个 run 中的所有 “不新鲜” 的 Entry 类实例
+         *（这个功能其实是由该方法中的 cleanSomeSlots(expungeStaleEntry(slotToExpunge), len); 这行代码实现的）
+         * 注意
+         * “清理” 就是指把该 Entry 类实例所在槽位置为 null
+         * 以便 GC 时回收 Entry 类实例中强引用（即 成员变量 value）所指向的那个对象
+         *（也就是 “所谓” 存放在 ThreadLocal 中的变量（因为当我们理解 ThreadLocal 类之后，就知道该变量存放在 ThreadLocal 中只是给人的外部感受而已））
+         * 以防止内存泄漏
+         *
+         *
+         * 上面注释中
+         * run 表示 Entry 数组（即成员变量 table）中，处于两个为 null 的槽位之间的那串子数组
+         *（这里的 null 是指 Entry 类实例为 null（即 当前这个槽位（下标）没有元素）和 “不新鲜” 的 Entry 类实例不是一个概念）
+         *
          * @param  key the key
+         *             该形参为当前这个 ThreadLocal 类实例
+         *            （即 ThreadLocalMap 类中的 set() 方法调用该方法时，传入的值（具体见 set() 方法中的形参介绍））
+         *
          * @param  value the value to be associated with key
+         *               该形参就是当前要往 ThreadLocal 中存放的变量（即 需要线程隔离的变量）
+         *
          * @param  staleSlot index of the first stale entry encountered while
          *         searching for key.
+         *                   该形参就是 ThreadLocalMap 类中的 set() 方法（即 当前方法的调用者）在变量寻找合适的槽位时
+         *                   遇到的第一个 “不新鲜” 的槽位
+         *                  （具体见该 set() 方法中的注释）
          */
         private void replaceStaleEntry(ThreadLocal<?> key, Object value,
                                        int staleSlot) {
@@ -527,18 +679,29 @@ public class ThreadLocal<T> {
             // We clean out whole runs at a time to avoid continual
             // incremental rehashing due to garbage collector freeing
             // up refs in bunches (i.e., whenever the collector runs).
+
+            // 该变量用于记录需要清理的 “不新鲜” 的 Entry 类实例所在槽位（下标）
             int slotToExpunge = staleSlot;
-            for (int i = prevIndex(staleSlot, len);
-                 (e = tab[i]) != null;
-                 i = prevIndex(i, len))
+
+            // 下面两个 for 循环的目的
+            // 是为了找出以当前 “不新鲜” 的槽位（即 下标）为原点，向前，以及向后遍历容器 table（即 ThreadLocalMap 的成员变量）
+            // 找出 并且 “清理” 当前 run（它的含义见该方法上面的注释）中，所有 “不新鲜” 的槽位
+
+            // 以当前 “不新鲜” 的槽位（即 下标）为原点，向前遍历容器 table（即 ThreadLocalMap 的成员变量）
+            for (int i = prevIndex(staleSlot, len); (e = tab[i]) != null; i = prevIndex(i, len))
+                // 找到 “不新鲜” 的 Entry 类实例之后，记录下它的槽位（即 下标）
+                // 如果遍历的过程中遇到多个 “不新鲜” 的 Entry 类实例
+                // 那么
+                // 在该 for 循环结束之后，变量 slotToExpunge 所记录的就是槽位（下标）最小的那个 “不新鲜” 的 Entry 类实例
                 if (e.get() == null)
                     slotToExpunge = i;
 
             // Find either the key or trailing null slot of run, whichever
             // occurs first
-            for (int i = nextIndex(staleSlot, len);
-                 (e = tab[i]) != null;
-                 i = nextIndex(i, len)) {
+
+            // 以当前 “不新鲜” 的槽位（即 下标）为原点，向后遍历容器 table（即 ThreadLocalMap 的成员变量）
+            for (int i = nextIndex(staleSlot, len); (e = tab[i]) != null; i = nextIndex(i, len)) {
+                // 获取当前 Entry 类实例所指向的 ThreadLocal 类实例
                 ThreadLocal<?> k = e.get();
 
                 // If we find key, then we need to swap it
@@ -546,16 +709,44 @@ public class ThreadLocal<T> {
                 // The newly stale slot, or any other stale slot
                 // encountered above it, can then be sent to expungeStaleEntry
                 // to remove or rehash all of the other entries in run.
+
+                // 下面的 if 语句中有一段元素交换的代码，原因见上面的英文注释
+
+                // 如果该 ThreadLocal 类实例就是当前这个 ThreadLocal 类实例
+                //（说明此时调用这个 set() 方法是为了替换原先存放在这个 ThreadLocal 中的变量）
                 if (k == key) {
+                    // 使用新值（即 当前调用该 set() 方法需要设置的值（即 当要存放到 ThreadLocal 中的值））覆盖旧值
                     e.value = value;
 
+                    // 就是这里进行的元素交换
+                    // 交换完成之后
+                    // 原先 “不新鲜” 的 Entry 类实例所在的槽位，就变成了
+                    // 新的值（即当前调用该 set() 方法所要设置的值（即 当要存放到 ThreadLocal 中的值））转换成一个新的 Entry 类实例了
+                    // 而当前遍历到的槽位（即 tab[i]）里存放的就变成了 “不新鲜” 的 Entry 类实例了
                     tab[i] = tab[staleSlot];
                     tab[staleSlot] = e;
 
                     // Start expunge at preceding stale entry if it exists
+                    // 这里判断这两个变量是否相等
+                    // 如果是
+                    // 那么就表示上那个向前遍历的 for 循环没有找到其他的 “不新鲜” 的 Entry 类实例
                     if (slotToExpunge == staleSlot)
+                        // 由于上面进行了元素交换，所以当前 i 所表示的槽位（下标）才是 “不新鲜” 的 Entry 类实例所在下标
+                        // 因此
+                        // 这里要把该 i 的值赋值给该 slotToExpunge 变量
                         slotToExpunge = i;
+
+                    // 下面的代码就是要清理所有的 “不新鲜” 的 Entry 类实例了
+
+                    // 这行代码的功能就是清理当前这个 run （它的含义见该方法上面的注释）中的所有 “不新鲜” 的 Entry 类实例
+                    // 注意
+                    // “清理” 就是指把该 Entry 类实例所在槽位置为 null
+                    // 以便 GC 时回收 Entry 类实例中强引用（即 成员变量 value）所指向的那个对象
+                    //（也就是 “所谓” 存放在 ThreadLocal 中的变量（因为当我们理解 ThreadLocal 类之后，就知道该变量存放在 ThreadLocal 中只是给人的外部感受而已））
+                    // 以防止内存泄漏
                     cleanSomeSlots(expungeStaleEntry(slotToExpunge), len);
+
+                    // 完成后直接返回
                     return;
                 }
 
@@ -572,6 +763,12 @@ public class ThreadLocal<T> {
 
             // If there are any other stale entries in run, expunge them
             if (slotToExpunge != staleSlot)
+                // 这行代码的功能就是清理当前这个 run （它的含义见该方法上面的注释）中的所有 “不新鲜” 的 Entry 类实例
+                // 注意
+                // “清理” 就是指把该 Entry 类实例所在槽位置为 null
+                // 以便 GC 时回收 Entry 类实例中强引用（即 成员变量 value）所指向的那个对象
+                //（也就是 “所谓” 存放在 ThreadLocal 中的变量（因为当我们理解 ThreadLocal 类之后，就知道该变量存放在 ThreadLocal 中只是给人的外部感受而已））
+                // 以防止内存泄漏
                 cleanSomeSlots(expungeStaleEntry(slotToExpunge), len);
         }
 
@@ -591,6 +788,11 @@ public class ThreadLocal<T> {
             int len = tab.length;
 
             // expunge entry at staleSlot
+            // “清理” 当前 “不新鲜” 的 Entry 类实例
+            // 即
+            // 把它的 Value（即 存放在 ThreadLocal 中的那个变量）置为 null（以便后期 GC 时回收该对象）
+            // 并且
+            // 把该槽位置为 null
             tab[staleSlot].value = null;
             tab[staleSlot] = null;
             size--;
@@ -598,9 +800,10 @@ public class ThreadLocal<T> {
             // Rehash until we encounter null
             Entry e;
             int i;
-            for (i = nextIndex(staleSlot, len);
-                 (e = tab[i]) != null;
-                 i = nextIndex(i, len)) {
+
+            // 该 for 循环用于遍历从当前槽位（即 形参 staleSlot 所对应的下标）开始，到下一个元素为 null 的槽位之间的子数组中的所有元素
+            // 清理该范围内的所有 “不新鲜” 的 Entry 类实例
+            for (i = nextIndex(staleSlot, len); (e = tab[i]) != null; i = nextIndex(i, len)) {
                 ThreadLocal<?> k = e.get();
                 if (k == null) {
                     e.value = null;
